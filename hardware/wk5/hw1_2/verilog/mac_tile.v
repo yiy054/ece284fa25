ea272febf87c7957bb0627ae3e7d97883f13316e
yi yao <yiy054@ucsd.edu>
2025-11-13 00:40:08 -0800

    mac_tile output

--- hardware/wk5/hw1_2/verilog/mac_tile.v
+++ hardware/wk5/hw1_2/verilog/mac_tile.v
@@ -5,11 +5,11 @@ module mac_tile (clk, out_s, in_w, out_e, in_n, inst_w, inst_e, reset);
 parameter bw = 4;
 parameter psum_bw = 16;
 
-output [psum_bw-1:0] out_s;
+output reg [psum_bw-1:0] out_s;
 input  [bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
-output [bw-1:0] out_e; 
+output reg [bw-1:0] out_e; 
 input  [1:0] inst_w;
-output [1:0] inst_e;
+output reg [1:0] inst_e;
 input  [psum_bw-1:0] in_n;
 input  clk;
 input  reset;
