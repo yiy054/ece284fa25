34ddf91f0c5292237ee6c613ebbe0402b2b6dee0
yi yao <yiy054@ucsd.edu>
2025-11-26 22:17:54 -0800

    Check runable

--- hardware/wk6/hw1/verilog/l0.v
+++ hardware/wk6/hw1/verilog/l0.v
@@ -14,45 +14,78 @@ module l0 (clk, in, out, rd, wr, o_full, reset, o_ready);
   output o_full;
   output o_ready;
 
-  wire [row-1:0] empty;
-  wire [row-1:0] full;
-  reg [row-1:0] rd_en;
+   parameter row  = 8;
+   parameter bw = 4;
+   // read_mode: 1 => read all rows at once; 2 => read one row at a time (rotating)
+   parameter read_mode = 1;
   
-  genvar i;
-
-  assign o_ready = ?? ;
-  assign o_full  = ?? ;
-
-
-  for (i=0; i<row ; i=i+1) begin : row_num
-      fifo_depth64 #(.bw(bw)) fifo_instance (
-	 .rd_clk(clk),
-	 .wr_clk(clk),
-	 .rd(rd_en[i]),
-	 .wr(...),
-         .o_empty(...),
-         .o_full(...),
-	 .in(...),
-	 .out(...),
-         .reset(reset));
-  end
-
-
-  always @ (posedge clk) begin
-   if (reset) begin
-      rd_en <= 8'b00000000;
+   input  clk;
+   input  wr;
+   input  rd;
+   input  reset;
+   input  [row*bw-1:0] in;
+   output [row*bw-1:0] out;
+   output o_full;
+   output o_ready;
+  
+   wire [row-1:0] empty;
+   wire [row-1:0] full;
+   reg [row-1:0] rd_en;
+   reg [31:0] rd_ptr; // pointer for version2 (only lower bits used)
+  
+   genvar i;
+  
+   // o_ready: at least one FIFO has room (not full)
+   assign o_ready = |(~full);
+   // o_full: enabled if any of the slots are full
+   assign o_full  = |(full);
+  
+  
+   for (i=0; i<row ; i=i+1) begin : row_num
+         fifo_depth64 #(.bw(bw)) fifo_instance (
+ 	 .rd_clk(clk),
+ 	 .wr_clk(clk),
+ 	 .rd(rd_en[i]),
+ 	 .wr(wr),
+               .o_empty(empty[i]),
+               .o_full(full[i]),
+ 	 .in(in[ i*bw +: bw ]),
+ 	 .out(out[ i*bw +: bw ]),
+               .reset(reset));
    end
-   else
-
-      /////////////// version1: read all row at a time ////////////////
-      ...
-      ///////////////////////////////////////////////////////
-
-
-
-      //////////////// version2: read 1 row at a time /////////////////
-      ...
-      ///////////////////////////////////////////////////////
+  
+  
+   always @ (posedge clk) begin
+    if (reset) begin
+         rd_en <= 8'b00000000;
+         rd_ptr <= 0;
     end
-
+    else begin
+         if (read_mode == 1) begin
+            // version1: read all rows at a time when rd asserted
+            if (rd)
+               rd_en <= 8'b11111111;
+            else
+               rd_en <= 8'b00000000;
+         end
+         else begin
+            // version2: rotate a one-hot read enable across rows while rd is asserted
+            if (rd) begin
+               // enable current row
+               rd_en <= (1 << rd_ptr) & (8'b11111111);
+               // increment pointer (wrap)
+               if (rd_ptr == row-1)
+                  rd_ptr <= 0;
+               else
+                  rd_ptr <= rd_ptr + 1;
+            end
+            else begin
+               // when rd deasserted, disable all and reset pointer
+               rd_en <= 8'b00000000;
+               rd_ptr <= 0;
+            end
+         end
+    end
+   end
+  
 endmodule
--- hardware/wk6/hw2/verilog/ofifo.v
+++ hardware/wk6/hw2/verilog/ofifo.v
@@ -6,35 +6,38 @@ module ofifo (clk, in, out, rd, wr, o_full, reset, o_ready, o_valid);
   parameter bw = 4;
 
   input  clk;
-  input  [??] wr;
+  input  [col-1:0] wr;
   input  rd;
   input  reset;
-  input  [??] in;
-  output [??] out;
+  input  [col*bw-1:0] in;
+  output [col*bw-1:0] out;
   output o_full;
   output o_ready;
   output o_valid;
 
-  wire [??] empty;
-  wire [??] full;
+  wire [col-1:0] empty;
+  wire [col-1:0] full;
   reg  rd_en;
   
   genvar i;
 
-  assign o_ready = ?? ;
-  assign o_full  = ?? ;
-  assign o_valid = ?? ;
+  // o_ready: there is at least one column with room (not full)
+  assign o_ready = |(~full);
+  // o_full: any column is full
+  assign o_full  = |(full);
+  // o_valid: a full vector is ready when every column has at least one element (none empty)
+  assign o_valid = &(~empty);
 
   for (i=0; i<col ; i=i+1) begin : col_num
       fifo_depth64 #(.bw(bw)) fifo_instance (
 	 .rd_clk(clk),
 	 .wr_clk(clk),
-	 .rd(???),
-	 .wr(???),
-         .o_empty(???),
-         .o_full(???),
-	 .in(???),
-	 .out(???),
+ 	 .rd(rd_en),
+ 	 .wr(wr[i]),
+         .o_empty(empty[i]),
+         .o_full(full[i]),
+ 	 .in(in[i*bw +: bw]),
+ 	 .out(out[i*bw +: bw]),
          .reset(reset));
   end
 
@@ -44,8 +47,8 @@ module ofifo (clk, in, out, rd, wr, o_full, reset, o_ready, o_valid);
       rd_en <= 0;
    end
    else
-      
-     ????
+      // register rd so the FIFO rd is synchronous
+      rd_en <= rd;
  
   end
 
