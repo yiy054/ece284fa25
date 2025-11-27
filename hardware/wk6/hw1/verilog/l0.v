9cd4f5bdc2ae36028bf66a3e945ff69e4164f014
yi yao <yiy054@ucsd.edu>
2025-11-26 22:25:36 -0800

    Update

--- hardware/wk6/hw1/verilog/l0.v
+++ hardware/wk6/hw1/verilog/l0.v
@@ -2,20 +2,17 @@
 // Please do not spread this code without permission 
 module l0 (clk, in, out, rd, wr, o_full, reset, o_ready);
 
-  parameter row  = 8;
-  parameter bw = 4;
-
-  input  clk;
-  input  wr;
-  input  rd;
-  input  reset;
-  input  [row*bw-1:0] in;
-  output [row*bw-1:0] out;
-  output o_full;
-  output o_ready;
-
    parameter row  = 8;
    parameter bw = 4;
+
+   input  clk;
+   input  wr;
+   input  rd;
+   input  reset;
+   input  [row*bw-1:0] in;
+   output [row*bw-1:0] out;
+   output o_full;
+   output o_ready;
    // read_mode: 1 => read all rows at once; 2 => read one row at a time (rotating)
    parameter read_mode = 1;
   
